2ea295ae4e570308f5c0e344f92f3f069da7b469
Jonsy13 <vedant.shrotria@chaosnative.com>
2021-06-03 19:44:25 +0530

    Added Job for Namespaced Scope
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>

--- litmus/uninstall-portal-namespaced.sh
+++ litmus/uninstall-portal-namespaced.sh
@@ -16,8 +16,8 @@ LITMUS_PORTAL_NAMESPACE=${PORTAL_NAMESPACE}
 # Shutting down the Litmus-Portal Setup
 kubectl delete -f https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/litmus-portal-crds.yml
 curl https://raw.githubusercontent.com/litmuschaos/litmus/master/docs/2.0.0-Beta/litmus-namespaced-2.0.0-Beta.yaml --output litmus-portal-namespaced-k8s-cleanup.yml
-envsubst < litmus-portal-namespaced-k8s-cleanup.yml > ${LITMUS_PORTAL_NAMESPACE}-ns-scoped-litmus-portal-cleanup.yml
+envsubst < litmus-portal-namespaced-k8s-cleanup.yml > "${LITMUS_PORTAL_NAMESPACE}-ns-scoped-litmus-portal-cleanup.yml"
 
-manifest_image_update $version ${LITMUS_PORTAL_NAMESPACE}-ns-scoped-litmus-portal-cleanup.yml
+manifest_image_update $version "${LITMUS_PORTAL_NAMESPACE}-ns-scoped-litmus-portal-cleanup.yml"
 
 kubectl delete -f ${LITMUS_PORTAL_NAMESPACE}-ns-scoped-litmus-portal-cleanup.yml -n ${LITMUS_PORTAL_NAMESPACE}
