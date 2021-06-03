c9d95c7a41aafb7539fa49d48fa1cddee21e7921
Jonsy13 <vedant.shrotria@chaosnative.com>
2021-06-03 18:28:07 +0530

    Added Job for Namespaced Scope
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>

--- litmus/install-portal-namespaced.sh
+++ litmus/install-portal-namespaced.sh
@@ -14,7 +14,7 @@ kubectl apply -f https://raw.githubusercontent.com/litmuschaos/litmus/master/lit
 curl https://raw.githubusercontent.com/litmuschaos/litmus/master/docs/2.0.0-Beta/litmus-namespaced-2.0.0-Beta.yaml --output litmus-portal-namespaced-k8s-setup.yml
 envsubst < litmus-portal-namespaced-k8s-setup.yml > ${LITMUS_PORTAL_NAMESPACE}-ns-scoped-litmus-portal-setup.yml
 
-manifest_image_update $version ${LITMUS_PORTAL_NAMESPACE}-ns-scoped-litmus-portal-setup.yml
+manifest_image_update $version "${LITMUS_PORTAL_NAMESPACE}-ns-scoped-litmus-portal-setup.yml"
 
 kubectl apply -f ${LITMUS_PORTAL_NAMESPACE}-ns-scoped-litmus-portal-setup.yml -n ${LITMUS_PORTAL_NAMESPACE}
 
