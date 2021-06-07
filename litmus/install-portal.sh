0b5733d6baffddac4ac5c0bd2fdd63eefda6fa72
Jonsy13 <vedant.shrotria@chaosnative.com>
2021-06-07 22:22:31 +0530

    Added Job for Namespaced Scope
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>

--- litmus/install-portal.sh
+++ litmus/install-portal.sh
@@ -65,5 +65,6 @@ else
     export INTERNAL_IP=$(kubectl -n litmus get nodes $NODE_NAME -o jsonpath='{.status.addresses[?(@.type=="InternalIP")].address}')
     export NODE_PORT=$(kubectl -n litmus get -o jsonpath="{.spec.ports[0].nodePort}" services litmusportal-frontend-service)
     Access_url="http://$INTERNAL_IP:$NODE_PORT"
+    echo "$Access_url"
     echo "URL=$Access_url" >> GITHUB_ENV
 fi
